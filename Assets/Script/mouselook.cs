bd58ed73623a36dd2a4d21dd56d2d353341e06f8
HugoProulx <48104950+HugoProulx@users.noreply.github.com>
2021-01-26 18:10:29 -0500

    Mouvement du personnage

---
+++ Assets/Script/mouselook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouselook : MonoBehaviour
+{
+    public float sensibiliterDeSouris = 100f;
+    public Transform corpJoueur;
+    float rotationX = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float sourisH = Input.GetAxis("Mouse X") * sensibiliterDeSouris * Time.deltaTime;
+        float sourisV = Input.GetAxis("Mouse Y") * sensibiliterDeSouris * Time.deltaTime;
+
+        rotationX -= sourisV;
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotationX, 0f, 0f);
+        corpJoueur.Rotate(Vector3.up * sourisH);
+    }
+}
---
+++ Assets/joueurMouvement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class joueurMouvement : MonoBehaviour
+{
+    public float vitesse = 12f;
+    public float graviter = -20f;
+    public float sautPuissance = 3f;
+
+
+    public Transform verificateurDeSol;
+    public float verificateurDistance = 0.4f;
+    public LayerMask solMask;
+
+    Vector3 velocity;
+    bool touchSol;
+
+    public CharacterController controller;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        touchSol = Physics.CheckSphere(verificateurDeSol.position, verificateurDistance, solMask);
+
+        if (touchSol && velocity.y < 0)
+            velocity.y = -1f;
+
+
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 mouvement = transform.right * h + transform.forward * v;
+
+        controller.Move(mouvement * vitesse * Time.deltaTime);
+
+
+        if (Input.GetButtonDown("Jump") && touchSol)
+        {
+            velocity.y = Mathf.Sqrt(sautPuissance * -2f * graviter);
+        }
+
+        velocity.y += graviter * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+         
+
+
+    }
+}
