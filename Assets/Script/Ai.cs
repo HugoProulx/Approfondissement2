da49191f6eeb47e827a1350acb77af89d5e5a5fd
HugoProulx <48104950+HugoProulx@users.noreply.github.com>
2021-02-01 09:25:39 -0500

    Début des animations, ajout d'un npc ainsi que ces mouvements aléatoire
    Ce push contient l'ajout des mouvements des npc ainsi que certaine animation pour les personnages ( personnage principale, ainsi que le npc)

---
+++ Assets/Script/Ai.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ai : MonoBehaviour
+{
+    public float vitesseDeMouvement = 3f;
+    public float vitesseDeRotation = 100f;
+
+    private bool marche = false;
+    private bool tourneGaucheGlobal = false;
+    private bool tourneDroiteGlobal = false;
+    private bool entrainDeMarcher = false;
+
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!marche)
+        {
+            //En utilisant le "StratCoroutine" cette technique est telle utiliser un multitread
+            //Ainsi, les mouvements de l'AI vont être plus fuild et non une constante activation de ces mouvements.
+            StartCoroutine(Marche());
+        }
+        if (tourneGaucheGlobal)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * vitesseDeRotation);
+        }
+        if (tourneDroiteGlobal)
+        {
+            transform.Rotate(transform.up * Time.deltaTime * -vitesseDeRotation);
+        }
+        if (marche)
+        {
+            transform.position+=(transform.forward * Time.deltaTime * vitesseDeMouvement);
+        }
+
+    }
+
+
+    IEnumerator Marche()
+    {
+        //Création de variable, ces variables ont pour utiliter de créer des valeurs aléatoires pour rendre les mouvements du personnages plus agérable visuellement grâce à l'aspect aléatoire.
+        int tempsDeRotation = Random.Range(1, 3);
+        int tempsEntreRotation = Random.Range(3, 5);
+        bool tourneGauche = 1 == Random.Range(1, 2);
+        int tempsDAttente = Random.Range(1, 4);
+        int tempsDeMarche = Random.Range(1, 5);
+
+        //En mettant "marche" à vrai, aucun autre "Tread" de marche va recommencer.
+        //Cela tant que marche va rester vrai
+        marche = true;
+
+        //yield return new WaitForSeconds permet de faire attendre le programme pour un temps donner.
+        yield return new WaitForSeconds(tempsDAttente);
+        entrainDeMarcher = true;
+        yield return new WaitForSeconds(tempsDeMarche);
+        entrainDeMarcher = false;
+        yield return new WaitForSeconds(tempsEntreRotation);
+        
+        if (tourneGauche)
+        {
+            tourneGaucheGlobal = true;
+            yield return new WaitForSeconds(tempsDeRotation);
+            tourneGaucheGlobal = false;
+        }
+        else
+        {
+            tourneDroiteGlobal = true;
+            yield return new WaitForSeconds(tempsDeRotation);
+            tourneDroiteGlobal = false;
+        }
+        marche = false;
+
+    }
+}
