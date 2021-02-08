4c6a2fac76252548af275b66d2033c43c5a1f8da
HugoProulx <48104950+HugoProulx@users.noreply.github.com>
2021-02-08 10:06:45 -0500

    Ajout d'un menu principal, ainsi qu'un menu de pause

---
+++ Assets/Script/ControleMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControleMenu : MonoBehaviour
+{
+    public void CommencerJeux()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); 
+    }
+    public void QuitterJeux()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/MenuPause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPause : MonoBehaviour
+{
+    public static bool jeuEnPause = false;
+    public GameObject menuDePause;
+    private int menuSceneID = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (jeuEnPause)
+            {
+                Resumer();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resumer()
+    {
+        menuDePause.SetActive(false);
+        Time.timeScale = 1f;
+        jeuEnPause = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        menuDePause.SetActive(true);
+        Time.timeScale = 0f;
+        jeuEnPause = true;
+    } 
+    public void MenuPrincipale()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(menuSceneID);
+    }
+    public void QuitterJeu()
+    {
+        Debug.Log("Jeu Quitter");
+        Application.Quit();
+    }
+
+}
